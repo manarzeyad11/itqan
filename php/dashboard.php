42153fca09c75c46a95c1f41f9f8f8e49eeb6cf4
manarzeyad11 <122107121+manarzeyad11@users.noreply.github.com>
2023-12-18 20:13:52 +0200

    UPDATE I

--- php/dashboard.php
+++ php/dashboard.php
@@ -5,22 +5,43 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
+require 'vendor/autoload.php';
+
+use ReallySimpleJWT\Token;
+
 $con = mysqli_connect("localhost", "root", "", "signup_db");
 
-$responce = array();
+$response = array();
 
+// Get token from the URL parameter
+$token = isset($_GET['token']) ? $_GET['token'] : null;
 
-// Get user ID from the URL parameter
-$user_id = isset($_GET['userId']) ? (int)$_GET['userId'] : 0;
+// Return the header claims
+Token::getHeader($token);
 
-// Validate user ID
-if ($user_id <= 0) {
-    echo "Invalid user ID.";
-    exit();
+// Return the payload claims
+$payload = Token::getPayload($token);
+
+// Access the 'user_id' from the payload
+$user_id = $payload['user_id'];
+
+$sql_token = "SELECT token_status FROM user_token WHERE token_string = '$token'";
+$result_token = mysqli_query($con, $sql_token);
+
+if ($result_token) {
+    $row_token = mysqli_fetch_assoc($result_token);
+
+    // Decode the JSON data
+    $token_status = $row_token['token_status'];
+
+    // Include token_status in the response
+    $response['token_status'] = $token_status;
+} else {
+    $response['error'] = "Error fetching token status: " . mysqli_error($con);
 }
 
 // Fetch dashboard data from the database
-$sql = "SELECT dashboard_data FROM dashboard WHERE user_id = $user_id";
+$sql = "SELECT dashboard_data FROM dashboard WHERE user_id = '$user_id'";
 $result = mysqli_query($con, $sql);
 
 if ($result) {
@@ -29,12 +50,13 @@ if ($result) {
     // Decode the JSON data
     $dashboard_data = json_decode($row['dashboard_data'], true);
 
-    echo json_encode([ $dashboard_data]);
-
-
+    // Include dashboard_data in the response
+    $response['dashboard_data'] = $dashboard_data;
 } else {
-    echo "Error fetching dashboard data: " . mysqli_error($conn);
+    $response['error'] = "Error fetching dashboard data: " . mysqli_error($con);
 }
 
+// Return the JSON response
+echo json_encode($response);
 
 ?>
--- php/login.php
+++ php/login.php
@@ -5,13 +5,13 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
-$con = mysqli_connect("localhost", "root", "", "signup_db");
-
-$responce = array();
-
 require 'vendor/autoload.php';
 
 use ReallySimpleJWT\Token;
+
+$con = mysqli_connect("localhost", "root", "", "signup_db");
+
+$responce = array();
 
 if ($con) {
     if (isset($_GET["username"]) && isset($_GET["password"])) {
@@ -24,27 +24,39 @@ if ($con) {
         $username = mysqli_real_escape_string($con, $username);
         $password = mysqli_real_escape_string($con, $password);
 
-        $sql = "SELECT user_id FROM users WHERE user_name = '$username' AND user_pass = '$password'";
-        $result = mysqli_query($con, $sql);
+        // Check if there is an existing token for the user
+        $existingTokenSql = "SELECT user_id, token_string FROM user_token WHERE user_id IN (SELECT user_id FROM users WHERE user_name = '$username' AND user_pass = '$password')";
+        $existingTokenResult = mysqli_query($con, $existingTokenSql);
 
-        $payload = ['username' => $username, 'time' => time()];
-        $secret = 'Hello&MikeFooBar123';
-
-        if ($result && $row = mysqli_fetch_assoc($result)) {
-            $user_id = $row['user_id'];
-            $token = Token::customPayload($payload, $secret);
-
-            // Insert the token into the user_token table
-            $insertTokenSql = "INSERT INTO user_token (user_id, token_string) VALUES ('$user_id', '$token')";
-            $insertTokenResult = mysqli_query($con, $insertTokenSql);
-
-            if ($insertTokenResult) {
-                echo json_encode(["login" => "Y", "user_id" => $user_id, "token" => $token]);
-            } else {
-                echo json_encode(["error" => "Failed to store token"]);
-            }
+        if ($existingTokenResult && $existingTokenRow = mysqli_fetch_assoc($existingTokenResult)) {
+            $user_id = $existingTokenRow['user_id'];
+            $existingToken = $existingTokenRow['token_string'];
+            echo json_encode(["login" => "Y", "user_id" => $user_id, "token" => $existingToken]);
         } else {
-            echo json_encode(["login" => "N"]);
+            // No existing token, generate a new one
+            $sql = "SELECT user_id FROM users WHERE user_name = '$username' AND user_pass = '$password'";
+            $result = mysqli_query($con, $sql);
+
+            if ($result && $row = mysqli_fetch_assoc($result)) {
+                $user_id = $row['user_id'];
+
+                $payload = ['user_id' => $user_id, 'username' => $username, 'time' => time()];
+                $secret = 'Hello&MikeFooBar123';
+
+                $token = Token::customPayload($payload, $secret);
+
+                // Insert the token into the user_token table
+                $insertTokenSql = "INSERT INTO user_token (user_id, token_string) VALUES ('$user_id', '$token')";
+                $insertTokenResult = mysqli_query($con, $insertTokenSql);
+
+                if ($insertTokenResult) {
+                    echo json_encode(["login" => "Y", "user_id" => $user_id, "token" => $token]);
+                } else {
+                    echo json_encode(["error" => "Failed to store token"]);
+                }
+            } else {
+                echo json_encode(["login" => "N"]);
+            }
         }
     } else {
         echo json_encode(["error" => "Missing parameters in the URL"]);

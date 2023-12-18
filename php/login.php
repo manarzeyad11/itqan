e2136a95007847cddb3b6968e0d7d7b581921723
manarzeyad11 <122107121+manarzeyad11@users.noreply.github.com>
2023-12-18 17:46:32 +0200

    UPDATE

--- php/login.php
+++ php/login.php
@@ -9,6 +9,10 @@ $con = mysqli_connect("localhost", "root", "", "signup_db");
 
 $responce = array();
 
+require 'vendor/autoload.php';
+
+use ReallySimpleJWT\Token;
+
 if ($con) {
     if (isset($_GET["username"]) && isset($_GET["password"])) {
         $username = $_GET["username"];
@@ -23,11 +27,23 @@ if ($con) {
         $sql = "SELECT user_id FROM users WHERE user_name = '$username' AND user_pass = '$password'";
         $result = mysqli_query($con, $sql);
 
+        $payload = ['username' => $username, 'time' => time()];
+        $secret = 'Hello&MikeFooBar123';
+
         if ($result && $row = mysqli_fetch_assoc($result)) {
             $user_id = $row['user_id'];
-            echo json_encode(["login" => "Y", "user_id" => $user_id]);
+            $token = Token::customPayload($payload, $secret);
+
+            // Insert the token into the user_token table
+            $insertTokenSql = "INSERT INTO user_token (user_id, token_string) VALUES ('$user_id', '$token')";
+            $insertTokenResult = mysqli_query($con, $insertTokenSql);
+
+            if ($insertTokenResult) {
+                echo json_encode(["login" => "Y", "user_id" => $user_id, "token" => $token]);
+            } else {
+                echo json_encode(["error" => "Failed to store token"]);
+            }
         } else {
-            
             echo json_encode(["login" => "N"]);
         }
     } else {

e3582e1e5c6a58e0024b6caff871059c4f58335c
manarzeyad11 <122107121+manarzeyad11@users.noreply.github.com>
2023-12-19 19:30:56 +0200

    UPDATE II

---
+++ database.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 19, 2023 at 06:30 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `signup_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_token`
+--
+
+CREATE TABLE `user_token` (
+  `token_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `token_string` varchar(255) DEFAULT NULL,
+  `token_status` varchar(50) DEFAULT 'not approved'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_token`
+--
+
+INSERT INTO `user_token` (`token_id`, `user_id`, `token_string`, `token_status`) VALUES
+(1, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwidGltZSI6MTcwMjkxNDI2OH0.Qzx0ekhnIMNk6NOeHMTRDd2aSjfyumgsIuaAkMNGeZI', 'approved'),
+(2, 23, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNhamEiLCJ0aW1lIjoxNzAyOTE0Mjk4fQ.juTxNUcJZUBrKVRf8mkxRDvAQ78r-kQt210k-IibqP4', 'not approved'),
+(7, 37, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMzciLCJ1c2VybmFtZSI6ImRhbmEiLCJ0aW1lIjoxNzAzMDA0NDI2fQ.E5dMfBrhbDBBED7F6UGsZ81dBBSzKasRPzlnHsSvRmg', 'approved'),
+(8, 39, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMzkiLCJ1c2VybmFtZSI6InNlZWwiLCJ0aW1lIjoxNzAzMDA2NzE3fQ.mFP5VKfjdGkI7h-K3QYrmqFN9QbZJJAdcyQ-3ClOfkI', 'not approved');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_token`
+--
+ALTER TABLE `user_token`
+  ADD PRIMARY KEY (`token_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_token`
+--
+ALTER TABLE `user_token`
+  MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_token`
+--
+ALTER TABLE `user_token`
+  ADD CONSTRAINT `user_token_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

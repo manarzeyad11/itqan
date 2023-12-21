299c54c7daac737ec5fd62adb11e8cb92facd5c2
manarzeyad11 <122107121+manarzeyad11@users.noreply.github.com>
2023-12-21 18:53:18 +0200

    UPDATE IV

--- php/dashboard.php
+++ php/dashboard.php
@@ -11,7 +11,7 @@ include 'connection.php';
 // Get token from the URL parameter
 $token = isset($_GET['token']) ? $_GET['token'] : null;
 
-$sql_token = "SELECT user_id, token_status FROM user_token WHERE token_string = '$token'";
+$sql_token = "SELECT user_id FROM user_token WHERE token_string = '$token'";
 $result_token = mysqli_query($con, $sql_token);
 
 if ($result_token) {
@@ -19,35 +19,44 @@ if ($result_token) {
 
     // Decode the JSON data
     $user_id = $row_token['user_id'];
-    $token_status = $row_token['token_status'];
 
-    if ($token_status !== "approved") {
-        // Token is not approved
-        $response['error'] = "Token not approved";
-    } else {
-        // Fetch dashboard data from the database
-        $sql = "SELECT dashboard_data FROM dashboard WHERE user_id = '$user_id'";
-        $result = mysqli_query($con, $sql);
+    // Fetch dashboard data from the database
+    $sql = "SELECT dashboard_data FROM dashboard WHERE user_id = '$user_id'";
+    $result = mysqli_query($con, $sql);
 
-        if ($result) {
-            $row = mysqli_fetch_assoc($result);
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
 
-            // Check if dashboard_data exists before decoding
-            if (isset($row['dashboard_data'])) {
-                // Decode the JSON data
-                $dashboard_data = json_decode($row['dashboard_data'], true);
-                $response = $dashboard_data;
-            } else {
-                $response['error'] = "No dashboard data found";
-            }
+        // Check if dashboard_data exists before decoding
+        if (isset($row['dashboard_data'])) {
+            // Decode the JSON data
+            $dashboard_data = json_decode($row['dashboard_data'], true);
+            $response = $dashboard_data;
         } else {
-            $response['error'] = "Error fetching dashboard data: " . mysqli_error($con);
+            $response['error'] = "No dashboard data found";
         }
+    } else {
+        $response['error'] = "Error fetching dashboard data: " . mysqli_error($con);
     }
-} else {
-    $response['error'] = "Error fetching token status: " . mysqli_error($con);
+
+    $sql_userInfo = "SELECT user_name , user_pass FROM users WHERE user_id = '$user_id'";
+    $result_userInfo = mysqli_query($con, $sql_userInfo);
+
+    if ($result_userInfo) {
+        $row_userInfo = mysqli_fetch_assoc($result_userInfo);
+
+        $user_name = $row_userInfo["user_name"];  
+        $user_pass = $row_userInfo["user_pass"];
+        
+    } else {
+        $response['error'] = "Error fetching user data: " . mysqli_error($con);
+    }
+
+
+
 }
 
 // Return the JSON response
-echo json_encode($response);
+echo json_encode(["user_name" => $user_name] + $response);
+
 ?>
